4671d0187e220dfd6bf3f13433c75fdde71a1711
ErnestoUlisesHM <netito.uli@icloud.com>
2025-09-24 12:24:20 -0600

    python file and pdf

---
+++ backpack.py
@@ -0,0 +1,29 @@
+# Practica 1 - Algoritmos Bioinspirados
+# Cornejo Morales Paola
+# Hernández Martínez Ernesto Ulises
+
+#   Vamos a abordar de problema de la mochila para el mundo de Harry Potter
+#       Hay 10 productos de cada tipo
+#       Capacidad de la mochila : 30 lb
+#   Restricciones
+#       Al menos 3 'Love Potion'
+#       Al menos 2 'Skiving Snackbox'
+
+#   Poblacion
+#       10 cromosomas
+#   Generaciones
+#       50 generaciones
+#   Probabilidad de cruza
+#       0.85
+#   Probabilidad de mutación
+#       0.1
+#   Selección de padres
+#       Ruleta
+#   Método de cruza
+#       Cruza uniforme (u =< 0.5)
+#   Método de mutación
+#       Mutación uniforme
+#   Selección de sobrevivientes
+#       Generación con reemplazo del padre más debil
+
+import random
