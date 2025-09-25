931489a82d3014e484b3cf4ba24654495a830731
ErnestoUlisesHM <netito.uli@icloud.com>
2025-09-25 14:23:38 -0600

    Intrucciones en el Python

--- backpack.py
+++ backpack.py
@@ -2,6 +2,8 @@
 # Cornejo Morales Paola
 # Hernández Martínez Ernesto Ulises
 
+import random
+
 #   Vamos a abordar de problema de la mochila para el mundo de Harry Potter
 #       Hay 10 productos de cada tipo
 #       Capacidad de la mochila : 30 lb
@@ -26,4 +28,26 @@
 #   Selección de sobrevivientes
 #       Generación con reemplazo del padre más debil
 
-import random
+#   PRODUCTOS
+#       Decoy Detonators
+#           Weight: 4 lb
+#           Price: 10 g
+#       Love Potion
+#           Weight: 2 lb
+#           Price: 8 g
+#       Extendable Ears
+#           Weight: 5 lb
+#           Price: 12 g
+#       Skiving Snackbox
+#           Weight: 5 lb
+#           Price: 6 g
+#       Fever Fudge
+#           Weight: 2 lb
+#           Price: 3 g
+#       Puking Pastilles
+#           Weight: 1.5 lb
+#           Price: 2 g
+#       Nosebleed Nougat
+#           Weight: 1 lb
+#           Price: 2 g
+
